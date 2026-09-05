148af90f0ea5408206e4f302465903f5a5dddfe4
NHGamer4 <aham.sharma.2009@gmail.com>
2026-09-06 02:17:57 +0530

    Array_problem 4

--- src/Array_problem4.java
+++ src/Array_problem4.java
@@ -1,4 +1,23 @@
 // Finding the maximum element in an array
 
+import java.util.Arrays;
+
 public class Array_problem4 {
+
+    public static int maximumFinder(int[] arr){
+        int num = 0;
+        for (int item: arr){
+            if(item>num){
+                num = item;
+            }
+        }
+        return num;
+    }
+
+    static void main() {
+
+        int[] arr = {2,6,8,4,9,3};
+        System.out.println((maximumFinder(arr)));
+    }
+
 }
