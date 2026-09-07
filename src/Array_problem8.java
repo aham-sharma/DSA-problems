c3f69b382fd23ef7a6ad05c8e306f4464e05adf7
NHGamer4 <aham.sharma.2009@gmail.com>
2026-09-07 20:53:37 +0530

    Array_problem 8 question

---
+++ src/Array_problem8.java
@@ -0,0 +1,16 @@
+//Swap alternate Element in an array
+
+public class Array_problem8 {
+
+    staic int[] alternateSwap(int[] arr){
+
+        int temp;
+
+        return
+    }
+
+    static void main() {
+
+    }
+
+}
