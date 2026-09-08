5026648e2298c34bf03d4f8ed9ede850501c7ae8
NHGamer4 <aham.sharma.2009@gmail.com>
2026-09-09 03:08:04 +0530

    Array_problem 9

---
+++ src/Array_problem9.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Array_problem9 {
+
+    static ArrayList<Integer> intersection(int[] arr, int[] brr){
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int item : arr){
+            for (int value : brr){
+                if (item == value){
+                    temp.add(value);
+                }
+            }
+        }
+
+        return temp;
+    }
+
+    static void main() {
+    int[] arr = {1,2,5,6,8,9};
+    int[] brr = {3,4,5,6,4,3};
+
+        System.out.println(intersection(arr,brr));
+    }
+
+}
