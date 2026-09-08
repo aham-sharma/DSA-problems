65d4e6f506b300a30c0fb4c410c652b803a4cbb4
NHGamer4 <aham.sharma.2009@gmail.com>
2026-09-09 02:42:51 +0530

    Array_problem 8

--- src/Array_problem8.java
+++ src/Array_problem8.java
@@ -1,16 +1,24 @@
 //Swap alternate Element in an array
 
+import java.util.Arrays;
+
 public class Array_problem8 {
 
-    staic int[] alternateSwap(int[] arr){
-
+    static int[] alternateSwap(int[] arr){
         int temp;
 
-        return
+        for (int i=0; i<arr.length-1 ; i+=2){
+        temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        }
+    return arr;
+
     }
 
-    static void main() {
+    static void main(){
 
-    }
-
+    int[] arr = {2,5,7,3,8,9,8};
+        System.out.println(Arrays.toString(alternateSwap(arr)));
+}
 }
