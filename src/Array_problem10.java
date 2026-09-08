8ec2b3fb6b5739388c5456cfbbced7063d922112
NHGamer4 <aham.sharma.2009@gmail.com>
2026-09-09 03:44:54 +0530

    Array_problem 10

---
+++ src/Array_problem10.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Array_problem10 {
+
+    static int[] alternateExtreme(int[] arr){
+        int temp;
+        for (int i = 0; i < (arr.length-1)/2; i++) {
+            temp = arr[i];
+            arr[i] = arr[(arr.length-1)-i];
+            arr[arr.length-1-i] = temp;
+        }
+        return arr;
+    }
+
+    static void main() {
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(alternateExtreme(arr)));
+    }
+
+}
