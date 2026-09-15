006deaec562e95c7db3366994636a3c060606785
NHGamer4 <aham.sharma.2009@gmail.com>
2026-09-16 00:01:46 +0530

    Array_problem 18

---
+++ src/Array_problem18.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Array_problem18 {
+
+        public static int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+
+                for (int j = i + 1; j < nums.length; j++) {
+
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+
+            return new int[]{};
+        }
+
+
+    static void main() {
+        int[] arr = {1,2,3,4,5,6,7};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(arr,target)));
+    }
+
+}
