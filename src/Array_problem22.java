fc6828ebad565c89e9115b99c4fb78eade584817
NHGamer4 <aham.sharma.2009@gmail.com>
2026-09-17 22:31:49 +0530

    Array_problem 22

---
+++ src/Array_problem22.java
@@ -0,0 +1,27 @@
+//find three numbers in array that adds up to be the target
+
+import java.util.Arrays;
+
+public class Array_problem22 {
+
+    static int[] threeSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++){
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == target) {
+                        return new int[]{i, j, k};
+                    }
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+    static void main() {
+            int[] arr = {1,2,3,4,5,6,7};
+            int target = 6;
+            System.out.println(Arrays.toString(threeSum(arr, target)));
+    }
+
+}
